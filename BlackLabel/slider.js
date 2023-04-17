a790c57935c10085e760385b3a300a18fb204549
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-04-17 10:41:40 -0500

    Add files via upload

--- BlackLabel/slider.js
+++ BlackLabel/slider.js
@@ -1,3 +1,35 @@
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section"); 
-let sliderSectionLast = slider__section[sliderSection.length -1]
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.maginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.maginLeft = "-100%";
+    }, 500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section"); 
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.maginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.maginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
