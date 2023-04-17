d0a9c77caa3d704459083a6db6480b73235e43ae
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-04-17 09:02:01 -0500

    Add files via upload

---
+++ BlackLabel/slider.js
@@ -0,0 +1,3 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section"); 
+let sliderSectionLast = slider__section[sliderSection.length -1]
